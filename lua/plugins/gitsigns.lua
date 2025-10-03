559e0eb77ea3c68c494b4ca9fca8942a4fc6c75d
Nick Barker <nbarker01@protonmail.com>
2025-10-03 08:15:43 -0400

    Added shortcut to see git diff

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,10 @@
 return {
 	'lewis6991/gitsigns.nvim',
 	commit = '1ee5c1fd068c81f9dd06483e639c2aa4587dc197',
+	opts = {
+    		on_attach = function(bufnr)
+		      local gitsigns = require("gitsigns")
+		      vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { buffer = bufnr })
+	    	end,
+  	},
 }
