a1b65c06a07e00687feb43ffbbc4f7d1a4fa3ffd
Nick Barker <nbarker01@protonmail.com>
2025-10-13 12:16:40 -0400

    Put telescope in deps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
   commit = DepsCommits.harpoon,
   dependencies = {
     { 'nvim-lua/plenary.nvim', commit = DepsCommits.plenary },
-    { 'nvim-telescope/telescope.nvim', commit = 'b4da76be54691e854d3e0e02c36b0245f945c2c7' },
+    { 'nvim-telescope/telescope.nvim', commit = DepsCommits.telescope },
   },
   keys = function()
     local harpoon = require('harpoon')
