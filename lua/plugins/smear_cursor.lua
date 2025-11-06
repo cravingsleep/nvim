a7d962bf364a028db3b87a0310288b3bfc33323e
Nick Barker <nbarker01@protonmail.com>
2025-11-05 22:32:48 -0500

    Added smear cursor and filtered marks to global only

---
+++ lua/plugins/smear_cursor.lua
@@ -0,0 +1,12 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+    smear_between_buffers = false,
+    time_interval = 7,
+    smear_to_cmd = false,
+    smear_insert_mode = false,
+  },
+}
